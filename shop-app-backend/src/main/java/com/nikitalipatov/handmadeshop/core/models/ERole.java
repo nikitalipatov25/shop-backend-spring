7f888a5389899df92148645ed5c8623bb6c3d6b3
stanislavbajkov <rt35xdh6ypo>
2021-09-04 13:55:05 +0300

    del

--- shop-app-backend/src/main/java/com/nikitalipatov/handmadeshop/core/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.nikitalipatov.handmadeshop.core.models;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- shop-app-backend/src/main/java/com/nikitalipatov/handmadeshop/core/repos/PromotionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nikitalipatov.handmadeshop.core.repos;
-
-import com.nikitalipatov.handmadeshop.core.models.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-}
